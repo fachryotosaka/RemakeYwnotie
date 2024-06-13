56398ad07389af69d6d2ce6e620e93f9fade3474
fachryotosaka <fachry2710@gmail.com>
2024-06-13 11:33:16 +0700

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,34 +8,19 @@ const content = `# About Me
 
 ![Samantha](https://imagedelivery.net/lLmNeOP7HXG0OqaG97wimw/clvlugru90000o4g8ahxp069s/db7abbe3-aa5c-433e-a16d-cbf137d1c9e5.png/public)
 
-Hey there! I'm Samantha, a 28-year-old former corporate warrior who decided to ditch the 9-to-5 grind and embark on an adventure of a lifetime. After years of hustling in a high-pressure job, I realized that life is too short to be stuck in an office, staring at spreadsheets all day.
-
-So I took a leap of faith, quit my cushy job in Singapore, and decided to see the world on my own terms. No more stuffy meetings or rigid schedules – just me, my backpack, and an open road ahead.
-
-![Samantha](https://imagedelivery.net/lLmNeOP7HXG0OqaG97wimw/clvlugru90000o4g8ahxp069s/6b080e65-2329-4a36-ad5c-0a6af8d9aeb1.png/public)
-
-This blog is where I'll be documenting my travels, sharing my experiences, and hopefully inspiring others to follow their wanderlust. From trekking through remote villages to savoring local cuisines, I'm on a mission to immerse myself in different cultures and create memories that will last a lifetime.
-
-But this journey isn't just about checking off destinations from a bucket list. It's about self-discovery, personal growth, and finding the courage to live life on my own terms. I'll be honest and raw, sharing the highs and lows, the moments of pure bliss and the inevitable challenges that come with solo travel.
-
-So join me on this adventure, and let's explore the world together! Who knows, maybe my stories will inspire you to take that leap of faith and pursue your own dreams, whatever they may be.
-
-Let's go on an adventure!
-
-Love,
-
-Samantha`;
+Hi there, I'm Fachry, though I am often called El. I am 18 years old and work as an investor, trader, and full-stack developer. I created this website to document everything about her ( Suci Nur Insani ) the most beautiful woman I have ever met. She is not only the most beautiful but also the most remarkable and extraordinary woman I have ever known. 
+Through this website, I aim to express all my feelings for her and share every detail about her. I hope that my words will convey the depth of my emotions for her.`;
 
 export async function generateMetadata() {
   return {
     title: "About Me",
-    description: "Learn more about Samantha and her travel adventures",
+    description: "Everything about her, the purest and most beautiful woman I have ever met.",
     openGraph: {
       title: "About Me",
-      description: "Learn more about Samantha and her travel adventures",
+      description: "Everything about her, the purest and most beautiful woman I have ever met.",
       images: [
         signOgImageUrl({
-          title: "Samantha",
+          title: "Ywnote",
           label: "About Me",
           brand: config.blog.name,
         }),
